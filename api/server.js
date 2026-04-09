f6e361dbec77c94fe41d03466308eed65caf7a37
alexisLAM01 <alexis.lamonnier@decathlon.com>
2026-04-09 09:09:49 +0200

    init: init commit

---
+++ api/server.js
@@ -0,0 +1,25 @@
+import express from "express";
+const postgres = require("postgres");
+const z = require("zod");
+
+const app = express();
+const port = 8000;
+const sql = postgres({ db: "mydb", user: "user", password: "password" });
+
+app.use(express.json());
+
+// Schemas
+const ProductSchema = z.object({
+  id: z.string(),
+  name: z.string(),
+  about: z.string(),
+  price: z.number().positive(),
+});
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Listening on http://localhost:${port}`);
+});
