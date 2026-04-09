e642bfbe60483ecf3128fef3843f8d63b1c3735c
alexkjzz <alex.lam@orange.fr>
2026-04-09 12:02:09 +0200

    feat: create-product

--- api/server.js
+++ api/server.js
@@ -4,16 +4,42 @@ const z = require("zod");
 
 const app = express();
 const port = 8000;
-const sql = postgres({ db: "mydb", user: "user", password: "password" });
+const sql = postgres({
+  host: "127.0.0.1",
+  db: "postgres",
+  user: "admin",
+  password: "password",
+  port: 5434
+});
 
 app.use(express.json());
 
 // Schemas
 const ProductSchema = z.object({
-  id: z.string(),
+  id: z.number(),
   name: z.string(),
   about: z.string(),
   price: z.number().positive(),
+});
+
+const CreateProductSchema = ProductSchema.omit({ id: true });
+
+app.post("/products", async (req, res) => {
+  const result = await CreateProductSchema.safeParse(req.body);
+
+  if (result.success) {
+    const { name, about, price } = result.data;
+
+    const product = await sql`
+   INSERT INTO products (name, about, price)
+   VALUES (${name}, ${about}, ${price})
+   RETURNING *
+   `;
+
+    res.send(product[0]);
+  } else {
+    res.status(400).send(result);
+  }
 });
 
 app.get("/", (req, res) => {
@@ -21,5 +47,5 @@ app.get("/", (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(`Listening on http://localhost:${port}`);
+  console.log(`Listening on http://127.0.0.1:${port}`);
 });
