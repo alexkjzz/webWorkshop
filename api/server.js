221e500121e933ce48b06672d08fc9bc37b0bc62
lucien-wrq <luluwerq@gmail.com>
2026-04-09 12:04:31 +0200

    add: route post product

--- api/server.js
+++ api/server.js
@@ -16,6 +16,27 @@ const ProductSchema = z.object({
   price: z.number().positive(),
 });
 
+const CreateProductSchema = ProductSchema.omit({ id: true });
+ 
+app.post("/products", async (req, res) => {
+  const result = await CreateProductSchema.safeParse(req.body);
+ 
+  // If Zod parsed successfully the request body
+  if (result.success) {
+    const { name, about, price } = result.data;
+ 
+    const product = await sql`
+    INSERT INTO products (name, about, price)
+    VALUES (${name}, ${about}, ${price})
+    RETURNING *
+    `;
+ 
+    res.send(product[0]);
+  } else {
+    res.status(400).send(result);
+  }
+});
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
